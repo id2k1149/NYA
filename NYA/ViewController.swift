0313c1a5658e7cb5670dc5d2a8727df69ad8b78e
id2k1149 <id2k1149@gmail.com>
2022-10-23 18:35:44 -0400

    add cornerRadius

--- NYA/ViewController.swift
+++ NYA/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var blackView: UIView!
+    
+    @IBOutlet var redView: UIView!
+    
+    @IBOutlet var yellowView: UIView!
+    
+    @IBOutlet var greenView: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        blackView.layer.cornerRadius = 100
+        redView.layer.cornerRadius = 100
+        yellowView.layer.cornerRadius = 100
+        greenView.layer.cornerRadius = 100
+        startButton.layer.cornerRadius = 25
+        
+        redView.isOpaque.toggle()
+        
+        
     }
 
-
+    @IBAction func startButtonWasTapped() {
+    }
+    
 }
 
