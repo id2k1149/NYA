6e60fad8b927a69f79357ef3f9e3fac61836e03a
id2k1149 <id2k1149@gmail.com>
2022-10-23 20:15:44 -0400

    - add enum LightIs - add switch to startButtonWasTapped()

--- NYA/ViewController.swift
+++ NYA/ViewController.swift
@@ -8,32 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var blackView: UIView!
     
     @IBOutlet var redView: UIView!
-    
     @IBOutlet var yellowView: UIView!
-    
     @IBOutlet var greenView: UIView!
+    
+    enum LightIs {
+        case red
+        case yellow
+        case green
+    }
+    
+    var light: LightIs = .red
     
     @IBOutlet var startButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        blackView.layer.cornerRadius = 100
-        redView.layer.cornerRadius = 100
-        yellowView.layer.cornerRadius = 100
-        greenView.layer.cornerRadius = 100
-        startButton.layer.cornerRadius = 25
+        blackView.layer.cornerRadius = 80
+        redView.layer.cornerRadius = 70
+        yellowView.layer.cornerRadius = 70
+        greenView.layer.cornerRadius = 70
         
-        redView.isOpaque.toggle()
-        
+        startButton.layer.cornerRadius = startButton.frame.height / 2
         
     }
 
     @IBAction func startButtonWasTapped() {
+        startButton.setTitle("NEXT", for: .normal)
+        
+        switch light {
+        case .red:
+            light = .yellow
+            redView.alpha = 0.3
+            yellowView.alpha = 1
+            greenView.alpha = 0.3
+        case .yellow:
+            light = .green
+            redView.alpha = 0.3
+            yellowView.alpha = 0.3
+            greenView.alpha = 1
+        case .green:
+            light = .red
+            redView.alpha = 1
+            yellowView.alpha = 0.3
+            greenView.alpha = 0.3
+        }
+        
     }
     
 }
